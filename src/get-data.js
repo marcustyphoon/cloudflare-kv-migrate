4f2c325ebfadbd709535a7287d877091059a1687
Marcus <marcustyphoon@gmail.com>
2024-07-27 23:21:49 -0700

    save values

--- src/get-data.js
+++ src/get-data.js
@@ -1,5 +1,36 @@
 import 'dotenv/config';
 import fs from 'fs/promises';
+import { uint8ArrayToBase64 } from 'uint8array-extras';
+
+const MAX_UPLOAD_REQUEST_SIZE = 1_000_000 * 100 * 0.8; // 100mb minus a bit
+const MAX_UPLOAD_KEYS = 10_000; // set 1000 for workers free tier limit
+
+const TEST_STOP_EARLY = Infinity;
+
+const apiFetch = async (url) => {
+  const result = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.GET_ACCOUNT_ID}${url}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.GET_BEARER}`,
+      },
+    },
+  ).then((result) => result.json());
+
+  if (!result.success) throw new Error(`fetch error on ${url}`);
+  return result;
+};
+
+const apiFetchRaw = async (url) => {
+  return fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.GET_ACCOUNT_ID}${url}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.GET_BEARER}`,
+      },
+    },
+  );
+};
 
 (async () => {
   await fs.mkdir('data-get').catch(() => {});
@@ -7,27 +38,23 @@ import fs from 'fs/promises';
   const dir = `data-get/${timestamp}`;
   await fs.mkdir(dir);
 
+  /**
+   * fetch keys
+   */
+
   const keys = [];
   let cursor;
 
   do {
     const queryParams = cursor ? `?cursor=${cursor}` : '';
-    const keysResponse = await fetch(
-      `https://api.cloudflare.com/client/v4/accounts/${process.env.GET_ACCOUNT_ID}/storage/kv/namespaces/${process.env.GET_KV_NAMESPACE_ID}/keys${queryParams}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.GET_BEARER}`,
-        },
-      },
-    ).then((result) => result.json());
+    const keysResponse = await apiFetch(
+      `/storage/kv/namespaces/${process.env.GET_KV_NAMESPACE_ID}/keys${queryParams}`,
+    );
 
     const {
-      success,
       result,
-      errors,
       result_info: { count, cursor: newCursor },
     } = keysResponse;
-    if (!success) throw new Error(errors);
     console.log(`fetched ${count} keys`);
 
     keys.push(...result);
@@ -38,4 +65,55 @@ import fs from 'fs/promises';
     encoding: 'utf8',
     flag: 'w+',
   });
+
+  /**
+   * fetch values and metadata
+   */
+
+  const allData = [[]];
+
+  for (const [i, { name: keyName }] of Object.entries(keys.slice(0, TEST_STOP_EARLY))) {
+    const metadataResponse = await apiFetch(
+      `/storage/kv/namespaces/${process.env.GET_KV_NAMESPACE_ID}/metadata/${keyName}`,
+    );
+    const metadata = metadataResponse.result;
+
+    // this endpoint has no wrapper? ok sure
+    const valueResponse = await apiFetchRaw(
+      `/storage/kv/namespaces/${process.env.GET_KV_NAMESPACE_ID}/values/${keyName}`,
+    );
+    const value = await valueResponse.arrayBuffer();
+
+    const dataValue = {
+      key: keyName,
+      base64: true,
+      value: uint8ArrayToBase64(new Uint8Array(value)),
+    };
+    if (metadata) {
+      dataValue.metadata = metadata;
+    }
+
+    const latestData = allData.at(-1);
+    const latestDataKeys = latestData.length;
+    const latestDataLength = JSON.stringify(latestData, null, 2).length;
+
+    if (latestDataKeys < MAX_UPLOAD_KEYS && latestDataLength < MAX_UPLOAD_REQUEST_SIZE) {
+      latestData.push(dataValue);
+    } else {
+      allData.push([dataValue]);
+    }
+
+    if (i % 5 === 0) {
+      console.log(`downloaded ${i} values`);
+    }
+  }
+
+  for (const [i, data] of Object.entries(allData)) {
+    await fs.writeFile(`${dir}/data${i}.json`, JSON.stringify(data, null, 2), {
+      encoding: 'utf8',
+      flag: 'w+',
+    });
+  }
+
+  console.log('saved all values!');
 })();
